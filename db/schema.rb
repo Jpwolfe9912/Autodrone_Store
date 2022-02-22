f1d98dfce8e83344861bc827fc42526864f210ca
Jeremy Wolfe <jpwolfe@me.com>
2022-02-21 22:51:21 -0500

    New files for Lab 4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_09_185518) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_22_025411) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_09_185518) do
     t.integer "stock"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
 end
